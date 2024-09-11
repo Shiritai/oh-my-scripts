fc4f813822f9c3b0a308cb961e5e0a4acc23f149
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-09-11 21:59:16 +0800

    Support `.dockerignore`
    `.dockerignore` will be generated from `docker-proto-ignore` and `.gitignore`.
    
    Please edit `docker-proto-ignore` for `.docker-ignore` only.

--- run.sh
+++ run.sh
@@ -4,22 +4,32 @@ IMG_NAME=oh-my-c
 USERNAME=$(id -un)
 script_dir=$(realpath $(dirname $0))
 
+# stack 1: ensure that we're running the script in correct directory
 OLD_DIR=$pwd
 cd $script_dir
 
-# zip scripts to a single file for image building
+# stack 2: zip scripts to a single file for image building
 zip -r scripts.zip scripts
 
+# stack 3: generate .dockerignore from docker-proto-ignore and .gitignore
+cat docker-proto-ignore .gitignore >> .dockerignore
+
+# build docker image
 sudo docker build -t $IMG_NAME \
                   --build-arg USERNAME="${USERNAME}" \
                   .
 
-# remove generated zip file
+# stack 3: remove temporary .dockerignore
+rm .dockerignore
+
+# stack 2: remove generated zip file
 rm scripts.zip
 
+# run container
 sudo docker run -d -it \
                 -v ./data:/home/${USERNAME}/data \
                 --name ${IMG_NAME} \
                 ${IMG_NAME} bash
 
+# stack 1: go back to old working directory
 cd $OLD_DIR
