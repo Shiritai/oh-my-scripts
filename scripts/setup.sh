f38260c6f53f2b8e8886ecabda628e1f3e073358
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-09-11 18:41:04 +0800

    Fix top `setup.sh` issue
    this change generalize the setup process without directly mentioning all the common plugins

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,11 @@
 
 script_dir=$(realpath $(dirname $0))
 
-$script_dir/zsh/setup.sh
+# run setup script in common directory
+for i in $(ls -d $script_dir/common/*/); do
+    ${i%%/}/setup.sh;
+done
+
 # run setup script in custom directory
 for i in $(ls -d $script_dir/custom/*/); do
     ${i%%/}/setup.sh;
