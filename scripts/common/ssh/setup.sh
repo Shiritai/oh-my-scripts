e8ed5ebfef714084b586e5565a7015ca70bd440c
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-02 04:56:44 +0800

    Enable ssh init

--- scripts/common/ssh/setup.sh
+++ scripts/common/ssh/setup.sh
@@ -5,3 +5,5 @@ install_if_dne openssh-server
 sudo sed -i "s/#Port.*/Port 22/" /etc/ssh/sshd_config
 sudo sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config
 sudo sed -i "s/#PasswordAuthentication.*/PasswordAuthentication yes/" /etc/ssh/sshd_config
+
+sudo systemctl enable ssh
