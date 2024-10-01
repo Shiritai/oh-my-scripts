742840ddacdf217b10cd4a85fa39c7020423b671
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-02 04:56:12 +0800

    New no-vnc with init support
    so we don't need `start.sh` anymore

--- scripts/common/no-vnc/setup.sh
+++ scripts/common/no-vnc/setup.sh
@@ -2,33 +2,12 @@
 
 SCRIPT_DIR=$(realpath $(dirname $0))
 
-# only install vnc dependency if using vnc
-if ! [ $USE_NO_VNC = "yes" ]; then
-    print_info "noVNC service is not needed"
-    exit 0
-fi
+print_info "setting up noVNC"
 
-print_info "noVNC service is needed, preparing vnc server..."
+install_if_dne git
 
-# Note: the length of password longer then 8
-#       will be truncated to the length of 8
-VNC_PSWD=${VNC_PSWD:-"vncpswd"}
-
-install_if_dne tightvncserver git libssl-dev
-
-mkdir $HOME/.vnc
-
-echo $VNC_PSWD | vncpasswd -f > $HOME/.vnc/passwd
-
-sudo chmod 600 $HOME/.vnc/passwd
-
-# Create an .Xauthority file
-touch $HOME/.Xauthority
-
-print_info "VNC environment is now set, setting up noVNC"
-
-cd ~/
-git clone https://github.com/novnc/noVNC.git
+cd /usr/share
+sudo git clone https://github.com/novnc/noVNC.git
 cd noVNC
 
 # Make sure that $SSL_REQ_CONFOG_FILE exists
@@ -37,6 +16,26 @@ if ! [[ -f $SSL_REQ_CONFOG_FILE ]]; then
     sudo cp $SSL_REQ_CONFOG_FILE.sample $SSL_REQ_CONFOG_FILE
 fi
 
-openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem -config $SSL_REQ_CONFOG_FILE
+sudo openssl req -new \
+                 -x509 \
+                 -days 365 \
+                 -nodes \
+                 -out self.pem \
+                 -keyout self.pem \
+                 -config $SSL_REQ_CONFOG_FILE
+
+echo "[Unit]
+Description=noVNC remote desktop server
+After=tigervnc@:1.service
+ 
+[Service]
+Type=simple
+User=${USER}
+ExecStart=/usr/share/noVNC/utils/novnc_proxy --vnc :5901 --listen 6901
+ 
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/novnc.service
+
+sudo systemctl enable novnc
 
 print_info "noVNC environment is now set"
