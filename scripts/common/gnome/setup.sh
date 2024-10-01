f96df455f47d73ed794dd0b7731fefaf23a9e130
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-02 07:06:10 +0800

    Make minimal gnome setup

--- scripts/common/gnome/setup.sh
+++ scripts/common/gnome/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-install_if_dne ubuntu-desktop \
-               fcitx-config-gtk \
-               gnome-usage
+# install_if_dne ubuntu-desktop-minimal
+install_if_dne gnome-session \
+               gnome-terminal \
+               gnome-control-center \
+               nautilus
 
 sudo apt-get purge -y --autoremove gnome-initial-setup
