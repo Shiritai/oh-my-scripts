aaf0f558c3b4ca578316532f967a750efbb2c84d
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-16 23:48:40 +0800

    Update code installer

--- scripts/app/code/setup.sh
+++ scripts/app/code/setup.sh
@@ -2,7 +2,10 @@
 
 print_info "Installing vscode app..."
 
-install_if_dne software-properties-common apt-transport-https wget
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-install_if_dne code
+# Ref: https://code.visualstudio.com/docs/setup/linux#_debian-and-ubuntu-based-distributions
+install_if_dne wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+sudo rm -f packages.microsoft.gpg
+install_if_dne apt-transport-https code
