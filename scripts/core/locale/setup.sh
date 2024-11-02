3d594d1c9000be8ce43b3d102e92a4e2b6f89c39
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-11-03 03:39:38 +0800

    Fix timezone bug

--- scripts/core/locale/setup.sh
+++ scripts/core/locale/setup.sh
@@ -2,10 +2,11 @@
 
 print_info "Setup locale to ${LOCALE} and timezone to ${TZ}"
 
-install_if_dne locales
+install_if_dne locales tzdata
 
 echo "$LOCALE UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 
 sudo ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime
 echo ${TZ} | sudo tee /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
